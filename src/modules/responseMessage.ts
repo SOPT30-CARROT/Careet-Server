7d0f93cff72fd11577a3749cacdd9d0dcbf7f812
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-24 16:41:25 +0900

    [FEAT] Add 'Get Popular Contents' responseMessage

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -2,7 +2,9 @@ const message = {
     NULL_VALUE: '필요한 값이 없습니다.',
     NOT_FOUND: '존재하지 않는 자원',
     BAD_REQUEST: '잘못된 요청',
-    INTERNAL_SERVER_ERROR: '서버 내부 오류'
+    INTERNAL_SERVER_ERROR: '서버 내부 오류',
+
+    GET_POPULAR_CONTENTS_SUCCESS: '인기 콘텐츠 조회 성공',
 }
 
 export default message;
