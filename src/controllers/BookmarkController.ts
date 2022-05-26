06214580f7ad95de69d938b327612f25f6915f72
Woojin Lee <dhkep03@gmail.com>
2022-05-26 17:24:16 +0900

    [CHORE] Reflect PR Review

--- src/controllers/BookmarkController.ts
+++ src/controllers/BookmarkController.ts
@@ -12,14 +12,11 @@ const toggleBookmark = async (req: Request, res: Response): Promise<void> => {
     const userId: string = params.userId;
 
     try {
-        // const bookmarkInfo: BookmarkInfo = 
         const data = await BookmarkService.toggleBookmark(contentId, userId);
-        console.log(data);
         if (data === "create") {
             res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_BOOKMARK_SUCCESS, data));    
-        } else 
-        if (data === "delete") {
-            res.status(statusCode.OK).send(util.success(statusCode.CREATED, message.DELETE_BOOKMARK_SUCCESS, data));    
+        } else if (data === "delete") {
+            res.status(statusCode.OK).send(util.success(statusCode.OK, message.DELETE_BOOKMARK_SUCCESS, data));    
         }
 
     } catch (error) {
@@ -32,10 +29,9 @@ const getContentBookmarked = async (req: Request, res: Response): Promise<void> 
     const { userId } = req.params;
 
     try {
-        // const bookmarkInfo: BookmarkInfo = 
         const data = await BookmarkService.getContentBookmarked(userId);
         
-        res.status(statusCode.OK).send(util.success(statusCode.CREATED, message.GET_BOOKMARKED_CONTENTS_SUCCESS, data));
+        res.status(statusCode.OK).send(util.success(statusCode.OK, message.GET_BOOKMARKED_CONTENTS_SUCCESS, data));
     } catch (error) {
         console.log(error);
         res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
